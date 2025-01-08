b2f3586d41cbf033a8db479b5818a13be37c4783
shaked1turgeman <shaked1turgeman@gmail.com>
2025-01-08 18:16:25 +0200

    test shaked

--- BLib_User/src/client/SubscriberUI.java
+++ BLib_User/src/client/SubscriberUI.java
@@ -4,7 +4,7 @@ import common.*;
 import gui.SubscriberEntryController;
 import javafx.application.Application;
 import javafx.stage.Stage;
-
+// het shaked
 public class SubscriberUI extends Application {
 	public static UserManager UM; //only one instance
 	public static Stage primaryStage ;
