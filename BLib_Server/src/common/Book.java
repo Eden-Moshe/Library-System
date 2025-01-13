ddf5fa870188c063a75b12f01566ccc20369d6c6
victor <Victor.Ryskin@e.braude.ac.il>
2025-01-13 17:24:01 +0200

    Revert "Merge remote-tracking branch 'origin/aviad's'"
    This reverts commit 3cc51c6cce7f0a7dc580adefb03317e991dc52f4, reversing
    changes made to d0b0b4e2085816f561eb028212dde93bd210a95c.

--- BLib_Server/src/common/Book.java
+++ BLib_Server/src/common/Book.java
@@ -1,97 +1,68 @@
 package common;
 
-import java.io.Serializable;
 import java.util.Date;
 
-public class Book implements Serializable {
-	private String barcode, bookName,bookGenre,bookDesc,shelf_location;
-	private boolean book_available;
-	private Date returnDate;
+public class Book {
+	private String bookName,bookSubject,barcode;
+	private int placeOnShelf;
+	private boolean bookExists;
+	private Date closestReturnDate;
 	
     // Constructor
-    public Book(String barcode, String bookName, String bookGenre,String bookDesc, String shelf_location, boolean book_available, Date returnDate) {
+    public Book(String bookName, String bookSubject, String barcode, int placeOnShelf, boolean bookExists, Date closestReturnDate) {
+        this.bookName = bookName;
+        this.bookSubject = bookSubject;
         this.barcode=  barcode;
-    	this.bookName = bookName;
-        this.bookGenre = bookGenre;
-        this.bookDesc=bookDesc;
-        this.shelf_location = shelf_location;
-        this.book_available = book_available;
-        this.returnDate = returnDate;
+        this.placeOnShelf = placeOnShelf;
+        this.bookExists = bookExists;
+        this.closestReturnDate = closestReturnDate;
     }
-    
-    // Getter for barcode
-    public String getBookBarcode() {
-        return barcode;
+
+    // Getter for bookName
+    public String getBookName() {
+        return bookName;
+    }
+
+
+    // Getter for bookSubject
+    public String getBookSubject() {
+        return bookSubject;
     }
     
     public void setBarcode(String barcode) {
         this.barcode = barcode;
     }
-    
-    // Getter for bookName
-    public String getBookName() {
-        return bookName;
-    }
-    
-    
-    // Setter for bookName
-    public void setBookName(String bookName) {
-        this.bookName=bookName;
-    }
-    
-
-    // Getter for bookGenre
-    public String getBookGenre() {
-        return bookGenre;
-    }
-    
-    // Setter for bookGenre
-    public void setBookGenre(String bookGenre) {
-        this.bookGenre=bookGenre;
-    }
-    
-    
-
-    // Getter for bookDesc
-    public String getBookDesc() {
-        return bookDesc;
-    }
 
     
-    // Setter for bookDesc
-    public void setBookDecs(String bookDesc) {
-        this.bookDesc=bookDesc;
-    }
-    
-
-    // Getter for placeOnShelf
-    public String getPlaceOnShelf() {
-        return shelf_location;
-    }
-    
-    // Setter for placeOnShelf
-    public void setPlaceOnShelf(String placeOnShelf) {
-        this.shelf_location = placeOnShelf;
+    // Getter for barcode
+    public String getBookBarcode() {
+        return barcode;
     }
 
-    // Getter for bookAvailable
-    public boolean isBookAvailable() {
-        return book_available;
+    // Getter and Setter for placeOnShelf
+    public int getPlaceOnShelf() {
+        return placeOnShelf;
     }
 
-    // Setter for bookAvailable
-    public void setBookAvailable(boolean book_available) {
-        this.book_available = book_available;
+    public void setPlaceOnShelf(int placeOnShelf) {
+        this.placeOnShelf = placeOnShelf;
     }
 
-    // Getter for returnDate
-    public Date getReturnDate() {
-        return returnDate;
+    // Getter and Setter for bookExists
+    public boolean isBookExists() {
+        return bookExists;
     }
 
-    // Setter for returnDate
-    public void setReturnDate(Date returnDate) {
-        this.returnDate = returnDate;
+    public void setBookExists(boolean bookExists) {
+        this.bookExists = bookExists;
     }
 
+    // Getter and Setter for closestReturnDate
+    public Date getClosestReturnDate() {
+        return closestReturnDate;
+    }
+
+    public void setClosestReturnDate(Date closestReturnDate) {
+        this.closestReturnDate = closestReturnDate;
+    }
 }
